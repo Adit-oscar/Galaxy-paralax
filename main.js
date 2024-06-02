1a897bf604cfe36440ca6f2ce7eb422db5c4d48c
Aditiya Warman <104644382+Adit-oscar@users.noreply.github.com>
2024-06-02 22:40:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const asteroid = document.getElementById("asteroid");
 window.addEventListener("scroll", function() {
     let scrollY = window.scrollY;
 
-    asteroid_2.style.top = 330 + scrollY * 0.5 / 4 + "px";                
-    asteroid_2.style.left = 200 + scrollY * -0.25 / 4 + "px";
+    asteroid_2.style.top = 330 + scrollY * .5 + "px";                
+    asteroid_2.style.left = 200 + scrollY * -.25 + "px";
     
-    roket.style.top = 650 + scrollY * -0.5 / 50 + "px";                
-    roket.style.right = 200 + scrollY * 0.25 / 50 + "px";
+    roket.style.top = 650 + scrollY * -0.5 / 5 + "px";                
+    roket.style.right = 200 + scrollY * 0.25 / 5 + "px";
 
-    bumi.style.top = 1250 + scrollY * -0.5 / 50 + "px";                
-    bumi.style.left = 280 + scrollY * -0.25 / 50 + "px";
+    bumi.style.top = 1250 + scrollY * -0.5 / 5 + "px";                
+    bumi.style.left = 280 + scrollY * -0.25 / 5 + "px";
     
-    asteroid.style.top = 1550 + scrollY * 0.5 / 50 + "px";                
-    asteroid.style.right = 250 + scrollY * 0.25 / 50 + "px";
+    asteroid.style.top = 1550 + scrollY * 0.5 / 5 + "px";                
+    asteroid.style.right = 250 + scrollY * 0.25 / 5 + "px";
 });
